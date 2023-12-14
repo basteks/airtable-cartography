382d83ffc096065bb66570f93093a7ae8e79aeb9
atelier des Basteks <64981563+basteks@users.noreply.github.com>
2023-12-15 00:23:45 +0100

    Update map_generation.js

--- map_generation.js
+++ map_generation.js
@@ -25,7 +25,7 @@ const config = input.config({
             parentTable: 'table',
         }),
         input.config.field('title', {
-            label: 'The field containing the title of each marker's popup',
+            label: "The field containing the title of each marker's popup",
             parentTable: 'table',
         }),
         input.config.select('group', {
@@ -49,7 +49,7 @@ const lat = config.lat;
 const lon = config.lon;
 const title = config.title;
 const group = config.group;
-const popupContent = config.popupContent;
+var popupContent = config.popupContent;
 const mapTitle = encodeURI(base.name+': '+table.name);
 
 function getFieldsFromPopupContent() {
@@ -104,61 +104,71 @@ function find(mtitle) {
     return res;
 };
 
-var fields=[];
-if(!getFieldsFromLabel()) {
-    fields=[];
-    output.markdown("Let's choose the fields to display on the map...");
-    var addMoreField = "Yes";
-    while(addMoreField=="Yes") {
-        let field = await input.fieldAsync("Choose a field you want to display", table);
-        fields.push(field);
-        let moreField = await input.buttonsAsync("Would you like to display another field on the map?", ['Yes', 'No']);
-        addMoreField = moreField;
-    }
-    output.markdown("**List of the fields to display on the map :**")
-    for (let i=0;i<fields.length;i++) {
-        output.markdown("_{"+fields[i].name+"}_")
-    }
-    const contentStr = await input.textAsync('Popup content to display for each record (you can use the fields' names between curly brackets)');
-    output.markdown("**Don't forget to copy/paste this text in the _Text to display in the popup for each record_ input in the script settings** (⚙ icon in the top right corner):");
-    output.text(contentStr);
+if (lat.type != 'number' || lon.type != 'number') {
+	output.markdown("**⚠ the latitude field and/or the longitude field are not of _number_ type! Please modify them or choose other number type fields**");
 }
+else {
+	var fields=[];
+	if(!getFieldsFromPopupContent()) {
+		fields=[];
+		output.markdown("Let's choose the fields to display on the map...");
+		var addMoreField = "Yes";
+		while(addMoreField=="Yes") {
+			let field = await input.fieldAsync("Choose a field you want to display", table);
+			fields.push(field);
+			let moreField = await input.buttonsAsync("Would you like to display another field on the map?", ['Yes', 'No']);
+			addMoreField = moreField;
+		}
+		output.markdown("**List of the fields to display on the map :**")
+		for (let i=0;i<fields.length;i++) {
+			output.markdown("_{"+fields[i].name+"}_")
+		}
+		const contentStr = await input.textAsync("Popup content to display for each record (you can use the fields' names between curly brackets)");
+		output.markdown("**Don't forget to copy/paste this text in the _Text to display in the popup for each record_ input in the script settings** (⚙ icon in the top right corner):");
+		output.text(contentStr);
+        popupContent = contentStr;
+	}
 
-var queryFields = fields;
-queryFields.push(title);
-queryFields.push(lat);
-queryFields.push(lon);
-let queryResult = await view.selectRecordsAsync({fields: queryFields});
+	var queryFields = fields;
+	queryFields.push(title);
+	queryFields.push(lat);
+	queryFields.push(lon);
+	let queryResult = await view.selectRecordsAsync({fields: queryFields});
 
-var markers=[];
+	var markers=[];
 
-for (let record of queryResult.records) {
-    let dataStr = replaceWithFieldNames(record,popupContent);
-    if (group=="yes") {
-        if (record.getCellValueAsString(title.name)!=""){
-            if (find(record.getCellValueAsString(title.name))>=0) {
-                markers[find(record.getCellValueAsString(title.name))].content.push(dataStr);
-            }
-            else {
-                markers.push({"title": record.getCellValueAsString(title.name), "lat" : record.getCellValue(lat.name), "lon" : record.getCellValue(lon.name),"content" : [dataStr]})
-            }
-        }
+	for (let record of queryResult.records) {
+		let dataStr = replaceWithFieldNames(record,popupContent);
+		if (group=="yes") {
+			if (record.getCellValueAsString(title.name)!=""){
+				if (find(record.getCellValueAsString(title.name))>=0) {
+					markers[find(record.getCellValueAsString(title.name))].content.push(dataStr);
+				}
+				else {
+					markers.push({"title": record.getCellValueAsString(title.name), "lat" : record.getCellValue(lat.name), "lon" : record.getCellValue(lon.name),"content" : [dataStr]})
+				}
+			}
+		}
+		else {
+			markers.push({"title": record.getCellValueAsString(title.name), "lat" : record.getCellValue(lat.name), "lon" : record.getCellValue(lon.name),"content" : [dataStr]})
+		}
+	}
+	var markersStr='';
+	for (let i=0;i<markers.length; i++) {
+		let marker = markers[i];
+		if (markersStr!='') { markersStr+=';'}
+		var listeData="";
+		for (let j=0;j<marker.content.length;j++) {
+			if (listeData!='') { listeData+=',';}
+			listeData += marker.content[j];
+		}
+		markersStr+= encodeURI(marker["title"]+','+marker["lat"]+","+marker["lon"]+","+listeData)
+	}
+	output.markdown("[Clic on this link to see the map]("+mapURL+"?t="+mapTitle+"?mrks="+markersStr+")");
+    let displayMarkers = await input.buttonsAsync("Would you like to display the markers' data?", ['Yes', 'No']);
+    if (displayMarkers == 'Yes') {
+        output.markdown("**List of the markers on the map**:");
+	    output.table(markers);
     }
-    else {
-        markers.push({"title": record.getCellValueAsString(title.name), "lat" : record.getCellValue(lat.name), "lon" : record.getCellValue(lon.name),"content" : [dataStr]})
-    }
+    output.markdown("_Script completed successfully_");
 }
-output.markdown("**List of the markers on the map**:");
-output.table(markers);
-var markersStr='';
-for (let i=0;i<markers.length; i++) {
-    let marker = markers[i];
-    if (markersStr!='') { markersStr+=';'}
-    var listeData="";
-    for (let j=0;j<marker.content.length;j++) {
-        if (listeData!='') { listeData+=',';}
-        listeData += marker.content[j];
-    }
-    markersStr+= encodeURI(marker["title"]+','+marker["lat"]+","+marker["lon"]+","+listeData)
-}
-output.markdown("[Clic on this link to see the map]("+mapURL+"?t="+mapTitle+"?mrks="+markersStr+")");
